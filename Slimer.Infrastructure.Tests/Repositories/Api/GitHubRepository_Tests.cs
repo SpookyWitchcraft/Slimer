5bc730f437aa22c0dab3f1017f2bf96d346ec23f
cap coldman <moctopus@outlook.com>
2023-09-11 10:33:23 -0400

    Adding moq Adding GitHubRepository tests

---
+++ Slimer.Infrastructure.Tests/Repositories/Api/GitHubRepository_Tests.cs
@@ -0,0 +1,65 @@
+﻿using Moq;
+using Slimer.Domain.Contracts.GitHub;
+using Slimer.Infrastructure.Modules.Api.Interfaces;
+using Slimer.Infrastructure.Repositories.Api;
+using Slimer.Infrastructure.Services.Interfaces;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Slimer.Infrastructure.Tests.Repositories.Api
+{
+    public class GitHubRepository_Tests
+    {
+        private readonly IHttpClientProxy _clientMock;
+        private readonly ISecretsService _secretsMock;
+
+        public GitHubRepository_Tests()
+        {
+            _clientMock = CreateHttpClinetProxyMock();
+            _secretsMock = CreateSecretServiceMock();
+        }
+
+        [Fact]
+        public void GitHubRepository_MissingClientShouldThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GitHubRepository(null!, _secretsMock));
+        }
+
+        [Fact]
+        public void GitHubRepository_MissingSecretServiceShouldThrow()
+        {
+            Assert.Throws<ArgumentNullException>(() => new GitHubRepository(_clientMock, null!));
+        }
+
+        [Fact]
+        public async Task CreateIssueAsync_ReturnsObject()
+        {
+            var repo = new GitHubRepository(_clientMock, _secretsMock);
+
+            var results = await repo.CreateIssueAsync(new GitHubRequest());
+
+            Assert.NotNull(results);
+        }
+
+        private static IHttpClientProxy CreateHttpClinetProxyMock()
+        {
+            var mock = new Mock<IHttpClientProxy>();
+
+            mock.Setup(x => x.SendAsync<GitHubResponse>(It.IsAny<HttpRequestMessage>()))
+                .ReturnsAsync(new GitHubResponse());
+
+            return mock.Object;
+        }
+
+        private static ISecretsService CreateSecretServiceMock()
+        {
+            var mock = new Mock<ISecretsService>();
+
+            mock.Setup(x => x.GetValue(It.IsAny<string>())).Returns("secret value");
+
+            return mock.Object;
+        }
+    }
+}
