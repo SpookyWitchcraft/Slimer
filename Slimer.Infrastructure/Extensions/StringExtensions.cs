67d79490a653388c643cdd028fc5de9591498be4
cap coldman <moctopus@outlook.com>
2023-09-14 14:48:25 -0400

    Updating chunking logic and adding more coverage

---
+++ Slimer.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Slimer.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static IList<string> ChunkWords(this string text)
+        {
+            var output = new List<string>();
+            var temp = "";
+
+            var split = text.Split(' ');
+
+            for (var i = 0; i < split.Length; i++)
+            {
+                temp = $"{temp.Trim()} {split[i]}";
+
+                if (temp.Length >= 128)
+                {
+                    output.Add(temp);
+
+                    temp = "";
+                }
+            }
+
+            if(!string.IsNullOrEmpty(temp))
+            {
+                output.Add(temp);
+            }
+
+            return output;
+        }
+    }
+}
