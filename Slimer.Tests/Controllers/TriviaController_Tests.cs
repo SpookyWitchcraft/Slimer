562c6ad4c158f2ad8e7fd44547f379f721626405
cap coldman <moctopus@outlook.com>
2023-09-20 16:03:21 -0400

    finishing triviacontroller_tests

--- Slimer.Tests/Controllers/TriviaController_Tests.cs
+++ Slimer.Tests/Controllers/TriviaController_Tests.cs
@@ -27,11 +27,133 @@ namespace Slimer.Tests.Controllers
         }
 
         [Fact]
-        public async Task TriviaController_GetShouldThrowBadRequest()
+        public async Task TriviaController_GetShouldReturn200()
+        {
+            var controller = new TriviaController(_serviceMock);
+
+            var results = await controller.Get() as OkObjectResult;
+
+            var response = results?.Value as TriviaQuestion;
+
+            Assert.NotNull(results);
+            Assert.NotNull(response);
+            Assert.True(results?.StatusCode == 200);
+            Assert.Equal(1, response.Id);
+            Assert.Equal("that", response.Answer);
+            Assert.Equal("what?", response.Question);
+            Assert.Equal("general", response.Category);
+            Assert.True(response.IsEnabled);
+            Assert.NotEqual(default, response.CreatedDate);
+            Assert.NotEqual(default, response.UpdatedDate);
+        }
+
+        [Fact]
+        public async Task TriviaController_GetWithIdShouldThrowBadRequest()
         {
             var controller = new TriviaController(_serviceMock);
 
             await Assert.ThrowsAsync<BadHttpRequestException>(() => controller.Get(0));
+        }
+
+        [Fact]
+        public async Task TriviaController_GetWithIdShouldReturn200()
+        {
+            var controller = new TriviaController(_serviceMock);
+
+            var results = await controller.Get(1) as OkObjectResult;
+
+            var response = results?.Value as TriviaQuestion;
+
+            Assert.NotNull(results);
+            Assert.NotNull(response);
+            Assert.True(results?.StatusCode == 200);
+            Assert.Equal(1, response.Id);
+            Assert.Equal("that", response.Answer);
+            Assert.Equal("what?", response.Question);
+            Assert.Equal("general", response.Category);
+            Assert.True(response.IsEnabled);
+            Assert.NotEqual(default, response.CreatedDate);
+            Assert.NotEqual(default, response.UpdatedDate);
+        }
+
+        [Fact]
+        public async Task TriviaController_SearchShouldReturn200()
+        {
+            var controller = new TriviaController(_serviceMock);
+
+            var results = await controller.Search() as OkObjectResult;
+
+            var response = results?.Value as IList<TriviaQuestion>;
+
+            Assert.NotNull(results);
+            Assert.NotNull(response);
+            Assert.True(results?.StatusCode == 200);
+            Assert.Equal(5, response.Count);
+            Assert.Equal("that", response[0].Answer);
+            Assert.Equal("what?", response[0].Question);
+            Assert.Equal("general", response[0].Category);
+            Assert.True(response[0].IsEnabled);
+            Assert.NotEqual(default, response[0].CreatedDate);
+            Assert.NotEqual(default, response[0].UpdatedDate);
+        }
+
+        [Fact]
+        public async Task TriviaController_SearchWithIdShouldReturn200()
+        {
+            var controller = new TriviaController(_serviceMock);
+
+            var results = await controller.Search(1) as OkObjectResult;
+
+            var response = results?.Value as TriviaQuestion;
+
+            Assert.NotNull(results);
+            Assert.NotNull(response);
+            Assert.True(results?.StatusCode == 200);
+            Assert.Equal(1, response.Id);
+            Assert.Equal("that", response.Answer);
+            Assert.Equal("what?", response.Question);
+            Assert.Equal("general", response.Category);
+            Assert.True(response.IsEnabled);
+            Assert.NotEqual(default, response.CreatedDate);
+            Assert.NotEqual(default, response.UpdatedDate);
+        }
+
+        [Fact]
+        public async Task TriviaController_PostShouldReturn200()
+        {
+            var controller = new TriviaController(_serviceMock);
+
+            var question = CreateTriviaQuestion(1);
+
+            var results = await controller.Post(question) as OkObjectResult;
+
+            var response = results?.Value as TriviaQuestion;
+
+            Assert.NotNull(results);
+            Assert.NotNull(response);
+            Assert.True(results?.StatusCode == 200);
+            Assert.Equal(question.Id, response.Id);
+            Assert.Equal(question.Answer, response.Answer);
+            Assert.Equal(question.Question, response.Question);
+            Assert.Equal(question.Category, response.Category);
+            Assert.Equal(question.IsEnabled, response.IsEnabled);
+            Assert.NotEqual(default, response.CreatedDate);
+            Assert.NotEqual(default, response.UpdatedDate);
+        }
+
+        [Fact]
+        public void TriviaController_InvalidateCacheShouldReturn200()
+        {
+            var controller = new TriviaController(_serviceMock);
+
+            var results = controller.Invalidate() as OkObjectResult;
+
+            var response = (bool)results?.Value;
+
+            Assert.NotNull(results);
+            Assert.NotNull(response);
+            Assert.True(results?.StatusCode == 200);
+            Assert.True(response);
         }
 
         private static ITriviaQuestionService CreateServiceMock()
@@ -39,24 +161,33 @@ namespace Slimer.Tests.Controllers
             var mock = new Mock<ITriviaQuestionService>();
 
             mock.Setup(x => x.GetRandomQuestionAsync())
-                .ReturnsAsync(new TriviaQuestion(1, "", "", "", true, DateTime.Now, DateTime.Now));
+                .ReturnsAsync(CreateTriviaQuestion(1));
 
-            mock.Setup(x => x.GetQuestionByIdAsync(It.IsAny<int>()))
-                .ReturnsAsync(new TriviaQuestion(1, "", "", "", true, DateTime.Now, DateTime.Now));
+            mock.Setup(x => x.GetQuestionByIdAsync(It.Is<int>(x => x == 1)))
+                .ReturnsAsync(CreateTriviaQuestion(1));
 
             mock.Setup(x => x.GetQuestionsAsync())
                 .ReturnsAsync(new Dictionary<int, TriviaQuestion>
                 {
-                    { 1, new TriviaQuestion(1, "", "", "", true, DateTime.Now, DateTime.Now) }
+                    { 1, CreateTriviaQuestion(1) },
+                    { 2, CreateTriviaQuestion(2) },
+                    { 3, CreateTriviaQuestion(3) },
+                    { 4, CreateTriviaQuestion(4) },
+                    { 5, CreateTriviaQuestion(5) }
                 });
 
             mock.Setup(x => x.SaveAsync(It.IsAny<TriviaQuestion>()))
-                .ReturnsAsync(new TriviaQuestion(1, "", "", "", true, DateTime.Now, DateTime.Now));
+                .ReturnsAsync(CreateTriviaQuestion(1));
 
             mock.Setup(x => x.InvalidateCache())
                 .Returns(true);
 
             return mock.Object;
+        }
+
+        private static TriviaQuestion CreateTriviaQuestion(int id)
+        {
+            return new TriviaQuestion(id, "what?", "that", "general", true, DateTime.Now, DateTime.Now);
         }
     }
 }
