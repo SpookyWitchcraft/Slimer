cedaff5b6dea235be192ddbe1906181719f98238
cap coldman <moctopus@outlook.com>
2024-06-13 14:55:45 -0400

    not sure what's up

--- Dockerfile
+++ Dockerfile
@@ -20,8 +20,4 @@ RUN dotnet publish "Slimer/Slimer.csproj" -c Release -o /app
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app .
-ARG VAULT_URL
-ARG ISSUES_URL
-ENV E_VAULT_URL=${VAULT_URL}
-ENV E_ISSUES_URL=${ISSUES_URL}
 ENTRYPOINT ["dotnet", "Slimer.dll"]
---
+++ Slimer/Features/Health/GetHealthCheck.cs
@@ -0,0 +1,23 @@
+﻿using Slimer.Interfaces;
+
+namespace Slimer.Features.Health
+{
+    public static class GetHealthCheck
+    {
+        public static class GetTriviaQuestionById
+        {
+            public class Endpoint : IEndpoint
+            {
+                public void MapEndpoint(IEndpointRouteBuilder app)
+                {
+                    app.MapGet("api/health", Handler).AllowAnonymous();
+                }
+
+                public static IResult Handler()
+                {
+                    return Results.Ok();
+                }
+            }
+        }
+    }
+}
